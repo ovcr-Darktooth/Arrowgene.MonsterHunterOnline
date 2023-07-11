254ed3537c391df254c61992efd9f3bc17f1a1ce
ovcr_Darktooth <brandonguti@hotmail.fr>
2023-07-11 18:51:37 +0200

    Cleaning code

--- Arrowgene.MonsterHunterOnline.Service/CsProto/Handler/CsCmdBattleActorStopMoveHandler.cs
+++ Arrowgene.MonsterHunterOnline.Service/CsProto/Handler/CsCmdBattleActorStopMoveHandler.cs
@@ -5,10 +5,10 @@ using Arrowgene.MonsterHunterOnline.Service.CsProto.Structures;
 
 namespace Arrowgene.MonsterHunterOnline.Service.CsProto.Handler;
 
-public class CsCmdBattleActorStopmove : ICsProtoHandler
+public class CsCmdBattleActorStopMoveHandler : ICsProtoHandler
 {
     private static readonly ServiceLogger Logger =
-        LogProvider.Logger<ServiceLogger>(typeof(CsCmdBattleActorStopmove));
+        LogProvider.Logger<ServiceLogger>(typeof(CsCmdBattleActorStopMoveHandler));
 
 
     public CS_CMD_ID Cmd => CS_CMD_ID.CS_CMD_BATTLE_ACTOR_STOPMOVE;
@@ -18,17 +18,12 @@ public class CsCmdBattleActorStopmove : ICsProtoHandler
     {
         CSActorStopmove req = new CSActorStopmove();
         req.Read(packet.NewBuffer());
-
-        //req.MoveSpeed.x = 10;
-        Logger.Info($"stopmove:{client.State._spawnPlayer.NetObjId} sync:{req.SyncTime} loc:{req.Location} actorrot:{req.ActorRot}");
+      
+        //Logger.Info($"stopmove:{client.State._spawnPlayer.NetObjId} sync:{req.SyncTime} loc:{req.Location} actorrot:{req.ActorRot}");
         client.SendCsPacket(NewCsPacket.ActorStopmoveNtf(new CSActorStopmoveNtf()
         {
             NetObjId = client.State._spawnPlayer.NetObjId,
             ActorStopmove = req
         }));
-
-        //client.SendCsPacket(NewCsPacket.ActorMovestate(req));
-
-
     }
 }
